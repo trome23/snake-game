d962cfeefe9ef360921a311b680002f479a89a70
Tyson Romero <romennoodles23@yahoo.com>
2021-08-24 21:49:15 -0600

    random apple being generated

---
+++ README.md
@@ -0,0 +1,3 @@
+#Snake Game
+
+Made using Javascript/HTML/CSS.  Classic Snake game where you try to eat as many 'apples' as possible in order to grow!  The snake cannot run into itself or the walls, or it will lose. Click the Start/Restart button to give it a go!
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const grid = document.querySelector(".grid")
 const startBtn = document.querySelector("#start-btn")
 const score = document.querySelector("#score")
+const width = 10
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
-let width = 10
+let appleIndex = 0
 
 function createGrid() {
     //creating 100 divs 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < width*width; i++) {
         const square = document.createElement("div")  //<== creating element using js
         
-        square.classList.add('squareStyle')  //<== adding a class, to style to each newly created div
+        square.classList.add('squareStyle')  //<== adding a class, to style each newly created div
 
         grid.appendChild(square)  //<== put div elements into our grid
 
         squares.push(square)  //<== push square element into an array
     }
-
 }
 createGrid()
 
@@ -26,28 +26,33 @@ currentSnake.forEach(index => squares[index].classList.add('snake'))
 function move() {
     //how to know if snake is hitting any of the 4 walls
     if (
-        (currentSnake[0] + 10 >= 100 && direction === 10) ||
-        (currentSnake[0] % 10 === 0 && direction === -1) ||
-        (currentSnake[0] % 10 >= 9 && direction === +1) ||
-        (currentSnake[0] - 10 < 0 && direction === -10) 
+        (currentSnake[0] + width >= width*width && direction === width) || //<== hitting bottom
+        (currentSnake[0] % width === 0 && direction === -1) || //<== hitting left
+        (currentSnake[0] % width === width-1 && direction === 1) || //<== hitting right
+        (currentSnake[0] - width < 0 && direction === -width) || //<== hitting top
+        squares[currentSnake[0] + direction].classList.contains("snake") //hitting itself
     )
-    return clearInterval(timerID)
-    //remove last element from current snake array
-    const tail = currentSnake.pop()   
-    //remove styling from last element
-    squares[tail].classList.remove('snake')
-    //add square to direction snake is moving
-    currentSnake.unshift(currentSnake[0] + direction)
-    //add styling so we can see it move
-    squares[currentSnake[0]].classList.add('snake')
+    return clearInterval(timerID) //<== stop snake from moving
+
+    const tail = currentSnake.pop() //<==remove last element from current snake array
+    squares[tail].classList.remove('snake')  //<==remove styling from last element
+    currentSnake.unshift(currentSnake[0] + direction) //<== add square to direction snake is moving
+    squares[currentSnake[0]].classList.add('snake') //<== add styling so we can see it move
 }
 move()
 
-let timerID = setInterval(move, 1000)
+let timerID = setInterval(move, 1000)  //<==setting speed of snake in milliseconds and calling move() function
+
+function createApples() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length) //<== creates random # for apple on the grid
+    } while (squares[appleIndex].classList.contains("snake"))  // <== do not create apples where the snake is
+    squares[appleIndex].classList.add("apple")  //<== create apple in random spot w/o snake in it
+}
+createApples()
 
 function control(e) {
-    //grabbing event keys and setting the direction based on 'keydown' eventlistener
-    if(e.key === 'ArrowRight' ) {
+    if(e.key === 'ArrowRight' ) {     //<==grabbing event keys and setting the direction based on 'keydown' eventlistener
         direction = 1
     } else if(e.key === 'ArrowUp') {
         direction = - width
